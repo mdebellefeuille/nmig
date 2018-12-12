a66462c74c86eb72a383b0e7787b0d36e6a2ce4f
Anatoly <anatolyuss@gmail.com>
2018-12-12 23:29:08 +0200

    experimenting with data stream processing

--- src/DataLoader.ts
+++ src/DataLoader.ts
@@ -30,7 +30,11 @@ import * as extraConfigProcessor from './ExtraConfigProcessor';
 import BufferStream from './BufferStream';
 import * as path from 'path';
 import { PoolClient } from 'pg';
+import { PoolConnection } from 'mysql';
+import { Transform, TransformOptions, TransformCallback } from 'stream';
 const { from } = require('pg-copy-streams'); // No declaration file for module "pg-copy-streams".
+
+// process.env.UV_THREADPOOL_SIZE = '64'; // TODO: consider to remove.
 
 process.on('message', async (signal: any) => {
     const conv: Conversion = new Conversion(signal.config);
@@ -82,7 +86,13 @@ async function deleteChunk(conv: Conversion, dataPoolId: number, client: PoolCli
 /**
  * Builds a MySQL query to retrieve the chunk of data.
  */
-function buildChunkQuery(tableName: string, selectFieldList: string, offset: number, rowsInChunk: number): string {
+function buildChunkQuery(tableName: string, selectFieldList: string, offset: number, rowsInChunk: number, pkColumnName?: string ): string {
+    if (pkColumnName) {
+        // TODO: pass column aliases to selectFieldList.
+        // return `SELECT ${ selectFieldList } FROM (SELECT ${ pkColumnName } FROM \`${tableName}\` LIMIT ${ offset },${ rowsInChunk })`
+           //  + ` AS sub_${ tableName } INNER JOIN ${ tableName } AS main_${ tableName } on main_${ tableName }.id = sub_${ tableName }.id;`;
+    }
+
     return `SELECT ${ selectFieldList } FROM \`${ tableName }\` LIMIT ${ offset },${ rowsInChunk };`;
 }
 
@@ -118,18 +128,103 @@ async function populateTableWorker(
 ): Promise<void> {
     return new Promise<void>(async resolvePopulateTableWorker => {
         const originalTableName: string = extraConfigProcessor.getTableName(conv, tableName, true);
-        const sql: string = buildChunkQuery(originalTableName, strSelectFieldList, offset, rowsInChunk);
+        const pkColumn: any = null; // conv._dicTables[tableName].arrTableColumns.find((c: any) => c.Key === 'PRI' && c.Extra === 'auto_increment');
+        let sql: string = '';
+
+        if (pkColumn) {
+            const pkColumnName: string = pkColumn.Field;
+            sql = buildChunkQuery(originalTableName, strSelectFieldList, offset, rowsInChunk, pkColumnName);
+        } else {
+            sql = buildChunkQuery(originalTableName, strSelectFieldList, offset, rowsInChunk);
+        }
+
+        const dbAccess: DBAccess = new DBAccess(conv);
+        const mysqlClient: PoolConnection = await dbAccess.getMysqlClient();
+
+        const sqlCopy: string = `COPY "${ conv._schema }"."${ tableName }" FROM STDIN DELIMITER '${ conv._delimiter }' CSV;`;
+        const client: PoolClient = await dbAccess.getPgClient();
+        const copyStream: any = client.query(from(sqlCopy));
+
+        copyStream
+            .on('end', () => {
+                // COPY FROM STDIN does not return the number of rows inserted.
+                // But the transactional behavior still applies (no records inserted if at least one failed).
+                // That is why in case of 'on end' the rowsInChunk value is actually the number of records inserted.
+                processSend(new MessageToMaster(tableName, rowsInChunk, rowsCnt));
+                return deleteChunk(conv, dataPoolId, client).then(() => resolvePopulateTableWorker());
+            })
+            .on('error', (copyStreamError: string) => {
+                return processDataError(conv, copyStreamError, sql, sqlCopy, tableName, dataPoolId, client)
+                    .then(() => resolvePopulateTableWorker());
+            });
+
+        const streamTransform: Transform = new Transform({
+            objectMode: true,
+
+            transform(chunk: any, encoding: string, callback: TransformCallback): void {
+                chunk[`${ conv._schema }_${ originalTableName }_data_chunk_id_temp`] = dataPoolId; ////////////////
+
+                csvStringify([chunk], async (csvError: any, csvString: string) => {
+                    //
+                    const buffer: Buffer = Buffer.from(csvString, encoding);
+                    this._buffer = Buffer.concat([this._buffer, buffer]);////////////////////////////////////
+                    this.push(buffer);
+                    callback();
+                }, encoding);
+            },
+
+            flush(callback: TransformCallback): void {
+                callback(undefined, this._buffer);
+            }
+        });
+
+        mysqlClient
+            .query(sql)
+            .on('error', (err: string) => {
+                // Handle error, an 'end' event will be emitted after this as well
+                return processDataError(conv, err, sql, sqlCopy, tableName, dataPoolId, client)
+                    .then(() => resolvePopulateTableWorker());
+            })
+            .on('fields', fields => { // TODO: check what is this good for?
+                // the field packets for the rows to follow
+            })
+            .on('result', row => {
+                // Pausing the connnection is useful if your processing involves I/O
+                // mysqlClient.pause();
+
+                // processRow(row, function() {
+                    // mysqlClient.resume();
+                // })
+            })
+            // .stream({highWaterMark: 5})
+            .stream()
+            .pipe(streamTransform)
+            .pipe(copyStream);
+
+    /* return new Promise<void>(async resolvePopulateTableWorker => {
+        const originalTableName: string = extraConfigProcessor.getTableName(conv, tableName, true);
+        const pkColumn: any = conv._dicTables[tableName].arrTableColumns.find((c: any) => c.Key === 'PRI' && c.Extra === 'auto_increment');
+        let sql: string = '';
+
+        if (pkColumn) {
+            const pkColumnName: string = pkColumn.Field;
+            sql = buildChunkQuery(originalTableName, strSelectFieldList, offset, rowsInChunk);
+        } else {
+            sql = buildChunkQuery(originalTableName, strSelectFieldList, offset, rowsInChunk);
+        }
+
         const dbAccess: DBAccess = new DBAccess(conv);
         const logTitle: string = 'DataLoader::populateTableWorker';
+        // console.log(`Before MySQL ${tableName} ${new Date()}`); // ///////// TODO: remove asap. /////////////////
         const result: DBAccessQueryResult = await dbAccess.query(logTitle, sql, DBVendors.MYSQL, false, false);
-
+        // console.log(`After MySQL ${tableName} ${new Date()}`); // ///////// TODO: remove asap. /////////////////
         if (result.error) {
             return resolvePopulateTableWorker();
         }
 
         rowsInChunk = result.data.length;
         result.data[0][`${ conv._schema }_${ originalTableName }_data_chunk_id_temp`] = dataPoolId;
-
+        // console.log(`Before CSV ${tableName} ${new Date()}`); // ///////// TODO: remove asap. /////////////////
         csvStringify(result.data, async (csvError: any, csvString: string) => {
             if (csvError) {
                 await generateError(conv, `\t--[${ logTitle }] ${ csvError }`);
@@ -137,12 +232,15 @@ async function populateTableWorker(
             }
 
             const buffer: Buffer = Buffer.from(csvString, conv._encoding);
+            // console.log(`After CSV ${tableName} ${new Date()}`); // ///////// TODO: remove asap. /////////////////
             const sqlCopy: string = `COPY "${ conv._schema }"."${ tableName }" FROM STDIN DELIMITER '${ conv._delimiter }' CSV;`;
             const client: PoolClient = await dbAccess.getPgClient();
             const copyStream: any = client.query(from(sqlCopy));
             const bufferStream: BufferStream = new BufferStream(buffer);
 
             copyStream.on('end', () => {
+                // console.log(`After COPY ${tableName} ${new Date()}`); // ///////// TODO: remove asap. /////////////////
+
                 // COPY FROM STDIN does not return the number of rows inserted.
                 // But the transactional behavior still applies (no records inserted if at least one failed).
                 // That is why in case of 'on end' the rowsInChunk value is actually the number of records inserted.
@@ -159,8 +257,8 @@ async function populateTableWorker(
                 return processDataError(conv, bufferStreamError, sql, sqlCopy, tableName, dataPoolId, client)
                     .then(() => resolvePopulateTableWorker());
             });
-
+            // console.log(`Before COPY ${tableName} ${new Date()}`); // ///////// TODO: remove asap. /////////////////
             bufferStream.setEncoding(conv._encoding).pipe(copyStream);
-        }, conv._encoding);
+        }, conv._encoding); */
     });
 }
